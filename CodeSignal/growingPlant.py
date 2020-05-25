530616411fd7bdb7cd3da6f6af62fd5ece16fa90
SungJin <ksjksjwin@gmail.com>
2020-05-24 22:53:54 -0700

    submit prob & sol

---
+++ CodeSignal/growingPlant.py
@@ -0,0 +1,41 @@
+'''
+Caring for a plant can be hard work, but since you tend to it regularly, you have a plant that grows consistently. Each day, its height increases by a fixed amount represented by the integer upSpeed. But due to lack of sunlight, the plant decreases in height every night, by an amount represented by downSpeed.
+
+Since you grew the plant from a seed, it started at height 0 initially. Given an integer desiredHeight, your task is to find how many days it'll take for the plant to reach this height.
+
+Example
+
+For upSpeed = 100, downSpeed = 10, and desiredHeight = 910, the output should be
+growingPlant(upSpeed, downSpeed, desiredHeight) = 10.
+
+#	Day	Night
+1	100	90
+2	190	180
+3	280	270
+4	370	360
+5	460	450
+6	550	540
+7	640	630
+8	730	720
+9	820	810
+10	910	900
+
+The plant first reaches a height of 910 on day 10.
+
+Copyright to © 2020 BrainFights Inc. All rights reserved
+
+'''
+def growingPlant(upSpeed, downSpeed, desiredHeight):
+
+    current_height = 0
+    count_day = 0
+
+    while True:
+        current_height += upSpeed
+        count_day += 1
+        if current_height >= desiredHeight:
+            break
+
+        current_height -= downSpeed
+
+    return count_day
