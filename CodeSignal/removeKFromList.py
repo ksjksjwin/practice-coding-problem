962da48d22209d572764b18223ca20adb0c7c95e
SungJin <ksjksjwin@gmail.com>
2020-05-29 12:08:19 -0700

    submit prob & sol

---
+++ CodeSignal/removeKFromList.py
@@ -0,0 +1,50 @@
+'''
+Note: Try to solve this task in O(n) time using O(1) additional space, where n is the number of elements in the list, since this is what you'll be asked to do during an interview.
+
+Given a singly linked list of integers l and an integer k, remove all elements from list l that have a value equal to k.
+
+Example
+
+For l = [3, 1, 2, 3, 4, 5] and k = 3, the output should be
+removeKFromList(l, k) = [1, 2, 4, 5];
+For l = [1, 2, 3, 4, 5, 6, 7] and k = 10, the output should be
+removeKFromList(l, k) = [1, 2, 3, 4, 5, 6, 7].
+
+Copyright to © 2020 BrainFights Inc. All rights reserved
+
+'''
+
+# Singly-linked lists are already defined with this interface:
+# class ListNode(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.next = None
+#
+
+def removeKFromList(l, k):
+    #create a fake head node for first number == k
+    temp_head = ListNode(None)
+    #connect temporary head with original l linked list
+    temp_head.next = l
+    #linked list to loop through (beginning from the fake head node)
+    current = temp_head
+
+    while current:
+        while (current.next) and (current.next.value == k):
+            current.next = current.next.next
+        current = current.next
+
+    return temp_head.next
+
+
+#Solution from other user
+'''
+def removeKFromList(l, k):
+    c = l
+    while c:
+        if c.next and c.next.value == k:
+            c.next = c.next.next
+        else:
+            c = c.next
+    return l.next if l and l.value == k else l
+'''
