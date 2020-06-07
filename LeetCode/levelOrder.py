b015ba8fee00b101140e3d5e3f5babc6ac7adfef
SungJin <ksjksjwin@gmail.com>
2020-06-06 22:28:03 -0700

    submit prob and sol

---
+++ LeetCode/levelOrder.py
@@ -0,0 +1,47 @@
+'''
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+
+Copyright © 2020 LeetCode
+
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return root
+
+        res = []
+        queue = deque([root])
+
+        while queue:
+            levelVal = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                levelVal.append(node.val)
+            res.append(levelVal)
+
+        return res
