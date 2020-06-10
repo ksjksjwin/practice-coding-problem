63e20e54ecef43f2ef840cfc644c6cb263dff646
SungJin <ksjksjwin@gmail.com>
2020-06-10 12:09:25 -0700

    submit prob & sol

---
+++ LeetCode/searchInsert.py
@@ -0,0 +1,42 @@
+'''
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Example 1:
+
+Input: [1,3,5,6], 5
+Output: 2
+Example 2:
+
+Input: [1,3,5,6], 2
+Output: 1
+Example 3:
+
+Input: [1,3,5,6], 7
+Output: 4
+Example 4:
+
+Input: [1,3,5,6], 0
+Output: 0
+
+Copyright © 2020 LeetCode
+
+'''
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if target in nums:
+            return nums.index(target)
+        else:
+            if target < nums[0]:
+                return 0
+
+            elif target > nums[-1]:
+                return len(nums)
+
+            else:
+                for i in range(len(nums)):
+                    if target < nums[i]:
+                        return i
+
+        return
