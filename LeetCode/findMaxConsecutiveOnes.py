8deb08ec4834915b01d12e44d5b85770d9f8d166
SungJin <ksjksjwin@gmail.com>
2020-06-28 10:54:00 -0700

    submit prob & sol

---
+++ LeetCode/findMaxConsecutiveOnes.py
@@ -0,0 +1,30 @@
+'''
+Given a binary array, find the maximum number of consecutive 1s in this array.
+
+Example 1:
+Input: [1,1,0,1,1,1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1s.
+    The maximum number of consecutive 1s is 3.
+Note:
+
+The input array will only contain 0 and 1.
+The length of input array is a positive integer and will not exceed 10,000
+
+Copyright © 2020 LeetCode
+
+'''
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+
+        res = 0
+        count = 0
+
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                count += 1
+            else:
+                res = max(res,count)
+                count = 0
+
+        return max(res,count)
